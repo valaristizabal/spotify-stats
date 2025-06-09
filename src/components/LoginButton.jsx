7ed9ecabd3470696274d6cf52dfa3e955e986030
Valeria <05valarist@gmail.com>
2025-06-09 12:43:50 -0500

    (UPDATE) login básico PRUEBAS

---
+++ src/components/LoginButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function LoginButton(){
+    const clientId = 'afd8dc773e82437ea3c129a63c71dd2b'; // pon tu client ID real
+    const redirectUri = 'https://spotify-stats-five-eta.vercel.app/callback';
+    const scopes = 'user-top-read user-read-email';
+    const responseType = 'token';
+
+    const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}&response_type=${responseType}`;
+    return (
+        <button onClick={() => window.location = authUrl}>
+        Iniciar sesión con Spotify
+        </button>
+    );
+}
---
+++ src/views/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { LoginButton } from '../components/LoginButton.jsx';
+
+export function Login() {
+  return (
+    <div>
+      <h1>Bienvenido a Spotify Stats</h1>
+      <LoginButton />
+    </div>
+  );
+}
